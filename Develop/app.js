198367b5af4347042af5fa13990578f63807c206
Vanessa Pham <phamxvanessa@gmail.com>
2020-06-04 13:49:49 -0600

    finished app.js

--- Develop/app.js
+++ Develop/app.js
@@ -13,9 +13,10 @@ const render = require("./lib/htmlRenderer");
 const allEmployees = [];
 const idArr = [];
 
+function employeeCLI() {
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
-function createTeam(){
+    function createTeam(){
     inquirer.prompt([
         {
             type: 'list',
@@ -54,12 +55,7 @@ function createTeam(){
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
 
-function outputTeam(){
-    if (!fs.existsSync(OUTPUT_DIR)) {
-        fs.mkdirSync(OUTPUT_DIR)
-    }
-    fs.writeFileSync(outputPath, render(allEmployees), 'utf-8');
-}
+
 
 // HINT: each employee type (manager, engineer, or intern) has slightly different
 // information; write your code to ask different questions via inquirer depending on
@@ -155,6 +151,16 @@ function addManager(){
     })
 }
 
+function outputTeam(){
+    if (!fs.existsSync(OUTPUT_DIR)) {
+        fs.mkdirSync(OUTPUT_DIR)
+    }
+    fs.writeFileSync(outputPath, render(allEmployees), 'utf-8');
+}
+createTeam();
+
+}
+employeeCLI();
 // HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
 // and Intern classes should all extend from a class named Employee; see the directions
 // for further information. Be sure to test out each class and verify it generates an
